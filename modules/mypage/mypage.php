b6ee6615a30e9cb57acfc87c81054db2268e626b
Martin <martin@linux-doku.de>
2010-09-03 04:53:17 -0700

    Chaned old comment style ;-)

--- modules/mypage/mypage.php
+++ modules/mypage/mypage.php
@@ -1,53 +1,49 @@
-<?PHP
-/*
-
-This Module was developed by Martin Lantzsch
-@author: Martin Lantzsch
-@mail: martin@linux-doku.de
-@litotex.de username: Martin
-
-*/
+<?php
+/**
+ * This Module was developed by Martin Lantzsch
+ * @author: Martin Lantzsch
+ * @mail: martin@linux-doku.de
+ * @litotex.de username: Martin
+ */
 
 @session_start();
 $modul_name="mypage"; //NAME OF THE MODUL
 require("./../../includes/global.php");
 
-/* CHECK IF THE MODUL IS ACTIVE - IF NOT GIVE ERROR */
+// check if module is active
 if (is_modul_name_aktive($modul_name)==0){
 	show_error('MODUL_LOAD_ERROR','core');
 	exit();
 }
-	/* GET THE PAGE NAME VIA GET */
+	// get page name from url
 	$page = $db->escape_string($_REQUEST['page']);
 	
-	/* IF NO PAGE IS GIVEN USE DEFAULT */
-	if($page == "")
+	// if no page is given use default
+	if($page == '')
 	{
-	  $page = "main";
+	  $page = 'main';
 	}
 
-	/* READ THE PAGE */
+	// read page data
 	$sql = "SELECT * FROM cc".$n."_mypages WHERE link = '$page' AND active = '1'";
 	$query = $db->query($sql);
 	$page = $db->fetch_array($query);
 	
-	/* IF WE HAVE NO PAGE SAY IT */
+	// if we have no page give "404"
 	if($db->num_rows($query) == 0)
 	{
-	  $modul_mypage_name = "404";
-	  $modul_mypage_content = "No page found!";
+	  $modul_mypage_name = '404';
+	  $modul_mypage_content = 'No page found!';
 	}
 	
-	/* SET THE VARS */
+	// set vars to tpl
 	$modul_mypage_name = $page['name'];
 	$modul_mypage_content = $page['content'];
 	
-	/* GIVE THE CONTENT TO THE THEME */
+	// give it to the template object
 	$tpl->assign('MODUL_MYPAGE_NAME', $modul_mypage_name);
 	$tpl->assign('MODUL_MYPAGE_CONTENT', $modul_mypage_content);
 
-	/* LOAD THEME */
+	// load template
 	template_out('mypage.html',$modul_name);
 	exit();
-
-?>
